c11a0bc460247631a66101a18dea17c49184b5e6
Heinair512 <h.holzhueter@mailbox.org>
2025-06-04 18:25:23 +0200

    Create WorkspaceView component

---
+++ src/views/WorkspaceView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="workspace-view">
+    <h1>Workspace</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.workspace-view {
+  padding: 1rem;
+}
+</style>
